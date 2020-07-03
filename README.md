7dbf249f93ba2f6c1b6eaa3607a3e7b297760c37
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-07-02 23:47:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# E-Portfolio_Github
+ My First Webpage
---
+++ script.js
@@ -0,0 +1,8 @@
+// Navbar JS
+$(document).ready(function () {
+
+    //Buttons in header
+    $(".menu-toggle-container").click(function() {
+        $(".navbar-list").toggle();
+    });
+});
