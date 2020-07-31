4fae970c84333e037ad899aeea077d953d263933
KHuey-2290 <huey_karissa_yayoi@student.smc.edu>
2020-07-31 00:08:50 -0700

    C21: need to add more images for lightbox; need to add hover images; need to figure out what to do with "Inspiration page"

--- script.js
+++ script.js
@@ -42,11 +42,7 @@ $(document).ready(function () {
         if (n > slides.length) {slideIndex = 1}
         if (n < 1) {slideIndex = slides.length}
         for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-        }
-        for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+            slides[i].style.display = "none";
         }
         slides[slideIndex-1].style.display = "block";
-        dots[slideIndex-1].className += " active";
     }
